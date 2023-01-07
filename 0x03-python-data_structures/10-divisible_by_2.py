afa49b49e67e906119700ddfe213ed85614a36fe
Fisayo <fasuyifisayo@gmail.com>
2023-01-07 08:59:31 -0800

    committed

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    boolist = my_list[:]
-    for count, i in enumerate(my_list):
-        if i % 2 == 0:
-            boolist[count] = True
-        else:
-            boolist[count] = False
-    return(boolist)
-
+    blist = [a % 2 == 0 for a in my_list]
+    return blist
