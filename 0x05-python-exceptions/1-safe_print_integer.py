1535a9b1fb1a079b8d48e5707cc54f22a0bbef93
Fisayo <fasuyifisayo@gmail.com>
2023-01-23 14:44:31 -0800

    committed

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,6 +4,4 @@ def safe_print_integer(value):
         print("{:d}".format(value))
         return True
     except:
-        (ValueError, TypeError):
-            print("{} is nor an integer".format(value))
         return False
