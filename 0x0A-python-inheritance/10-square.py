4e5238c8ab3659834f69b98ffef86b37b5ba2ddc
Fisayo <fasuyifisayo@gmail.com>
2023-02-07 01:16:22 +0100

    committed

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -16,11 +16,5 @@ class Square(Rectangle):
     """ Square Class """
     def __init__(self, size):
         """ instantiation with size """
-        super().__init__(self, size)
-        self.integer_validator("size", size)
         self.__size = size
-
-    def area(self):
-        """rectangle area"""
-
-        return self.__size ** 2
+        super().__init__(self.__size, self.__size)
