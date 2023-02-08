34d7826454c57146302590632f64542dd5ca7fd9
Fisayo <fasuyifisayo@gmail.com>
2023-02-08 08:08:44 +0100

    committed

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
 
-import json
-
-
-def save_to_json_file(my_obj, filename):
-    with open(filename, "w", encoding="UTF-8") as f:
-        json.dump(my_obj, f)
+def read_file(filename=""):
+    with open(filename, "r", encoding="UTF-8") as f:
+        print(f.read(), end="")
