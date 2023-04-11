98e442b5347a49e6e6a717af91b8f98ab96458b5
Fisayo <fasuyifisayo@gmail.com>
2023-04-11 22:26:56 +0000

    committed

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -2,10 +2,11 @@
 const myObject = {
   type: 'object',
   value: 12,
-  incr: function () {
-    return this.value++;
-  }
 };
+console.log(myObject);
+myObject.incr = function(){
+return this.value++
+}
 myObject.incr();
 console.log(myObject);
 myObject.incr();
