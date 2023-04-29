1a2ff5026ca5812754e6fc45e70153e55f0676ed
Fisayo <fasuyifisayo@gmail.com>
2023-04-28 17:04:44 -0700

    committed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ fetches https://intranet.hbtn.io/status.
 import urllib.request
 if __name__ == '__main__':
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
         content = resp.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
