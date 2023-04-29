c7c15ede48da5880141b13155d5d7a1b2111e74f
Fisayo <fasuyifisayo@gmail.com>
2023-04-28 17:25:23 -0700

    committed

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
 Python script that takes your Github credentials (username and password)
-and uses the Github API to display your id
 """
 import requests
 from requests.auth import HTTPBasicAuth
